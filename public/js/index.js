a613e8ab82a60888ea084ce0f76166076bd34eec
Gavin Hung <gavinkhung@gmail.com>
2020-06-27 20:15:54 -0700

    finished hamburger menu

--- public/js/index.js
+++ public/js/index.js
@@ -9,6 +9,15 @@ $(document).ready(function(){
         .reverse(false)
         .addTo(controller);
     });
+
+    $('#hamburger-icon').click(function(){
+      let hamburgerMenu = document.querySelector('#hamburger-menu');
+      if (hamburgerMenu.style.display === "block") {
+        hamburgerMenu.style.display = "none";
+      } else {
+        hamburgerMenu.style.display = "block";
+      }
+    });
 });
 
 
