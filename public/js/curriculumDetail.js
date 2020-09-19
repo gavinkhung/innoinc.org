2c0f5277923bc76760e34c7363e5a60ec4daacfa
Gavin Hung <gavinkhung@gmail.com>
2020-09-19 13:58:46 -0700

    added embedded video player

--- public/js/curriculumDetail.js
+++ public/js/curriculumDetail.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+$(document).ready(() => {
     const db = firebase.firestore();
 
     const contentDiv = document.getElementById('curriculum-detail-content');
@@ -16,15 +16,25 @@ $(document).ready(function(){
         contentRef.get().then(contentSnapshot => {
             if(contentSnapshot && contentSnapshot.exists){                
 
-                const { title } = contentSnapshot.data();
+                const { title, embedID } = contentSnapshot.data();
 
                 const p = document.createElement('p');
                 p.innerHTML = title;
 
                 // 
-                const p = document.createElement('div');
+                const playerDiv = document.createElement('div');
+                playerDiv.setAttribute("id", "player");
+                playerDiv.classList.add('plyr__video-embed');
+
+                const iframe = document.createElement('iframe');
+                iframe.setAttribute("src", `https://www.youtube.com/embed/${embedID}?origin=https://plyr.io&amp;iv_load_policy=3&amp;modestbranding=1&amp;playsinline=1&amp;showinfo=0&amp;rel=0&amp;enablejsapi=1`);
+                iframe.setAttribute("allowfullscreen", "true");
+                iframe.setAttribute("allowtransparency", "true");
+
+                playerDiv.appendChild(iframe);
 
                 row.appendChild(p);
+                row.appendChild(playerDiv);
             } else {
                 const p = document.createElement('p');
                 p.innerHTML = "unable to find content in firestore";
