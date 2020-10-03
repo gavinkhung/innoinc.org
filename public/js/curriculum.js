789ad52bd5333050dc92f901ee0c31649891b8cd
Gavin Hung <gavinkhung@gmail.com>
2020-10-02 22:44:49 -0700

    put weeks into one section

--- public/js/curriculum.js
+++ public/js/curriculum.js
@@ -8,36 +8,40 @@ $(document).ready(function(){
             curriculumDocs.map(subjectDoc => {
                 const subjectName = subjectDoc.id;
                 const subjectId = `circulum-subjects-${subjectName}`;
-                const { subcollections } = subjectDoc.data(); 
+                const { subcollections } = subjectDoc.data();
                 if(subcollections){
+                    const row = document.createElement('div');
+                    row.classList.add('row');
+
+                    const headerRow = document.createElement('div');
+                    headerRow.classList.add('sixteen');
+                    headerRow.classList.add('wide');
+                    headerRow.classList.add('column');
+                    headerRow.classList.add('subject-name');
+                    const h2 = document.createElement('h2');
+                    h2.classList.add('ui');
+                    h2.classList.add('header');
+                    h2.innerHTML = subjectName;
+
+                    headerRow.appendChild(h2);
+                    row.appendChild(headerRow);
+
                     subcollections.map(subcollection => {
                         const { subcollectionName, title } = subcollection;
-
                         if(subcollectionName && title){
-                            const row = document.createElement('div');
-                            row.classList.add('row');
-
                             const listRow = document.createElement('div');
                             listRow.classList.add('sixteen');
                             listRow.classList.add('wide');
-                            listRow.classList.add('column');
-                            const headerRow = document.createElement('div');
-                            headerRow.classList.add('sixteen');
-                            headerRow.classList.add('wide');
-                            headerRow.classList.add('column');
-                            headerRow.classList.add('subject-name');
+                            listRow.classList.add('column');    
                             const subHeaderRow = document.createElement('div');
                             subHeaderRow.classList.add('sixteen');
                             subHeaderRow.classList.add('wide');
                             subHeaderRow.classList.add('column');
                             subHeaderRow.classList.add('subheader-name');
-                            const h2 = document.createElement('h2');
-                            h2.classList.add('ui');
-                            h2.classList.add('header');
-                            h2.innerHTML = subjectName;
                             const h3 = document.createElement('h3');
                             h3.classList.add('ui');
                             h3.classList.add('header');
+                            h3.classList.add('curriculum-mbs');
                             h3.innerHTML = title;
                             const list = document.createElement('div');
                             list.classList.add('ui');
@@ -46,11 +50,9 @@ $(document).ready(function(){
                             list.classList.add('list');
                             list.setAttribute('id', subjectId);
 
-                            headerRow.appendChild(h2);
                             subHeaderRow.appendChild(h3);
                             listRow.appendChild(list);
 
-                            row.appendChild(headerRow);
                             row.appendChild(subHeaderRow);
                             row.appendChild(listRow);
 
