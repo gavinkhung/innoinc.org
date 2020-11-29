b409ed6885ffb55ab726e69477c11da792eec805
Gavin Hung <gavinkhung@gmail.com>
2020-11-28 17:39:07 -0800

    separated curriculum list to curriculum detail pages

--- public/js/curriculumDetail.js
+++ public/js/curriculumDetail.js
@@ -1,49 +1,194 @@
-$(document).ready(() => {
-    const db = firebase.firestore();
+// $(document).ready(function(){
+//     const db = firebase.firestore();
+//     const curriculumRef = db.collection('curriculum');
+//     curriculumRef.get().then(curriculumSnapshot => {
+//         const curriculumDocs = curriculumSnapshot.docs;
+//         if(curriculumDocs){
+//             const circulumSubjects = document.getElementById('circulum-subjects');
+//             curriculumDocs.map(subjectDoc => {
+//                 const subjectName = subjectDoc.id;
+//                 const subjectId = `circulum-subjects-${subjectName}`;
+//                 const { subcollections } = subjectDoc.data();
+//                 if(subcollections){
+//                     const row = document.createElement('div');
+//                     row.classList.add('row');
 
-    const contentDiv = document.getElementById('curriculum-detail-content');
-    const headerRow = document.createElement("div");
-    headerRow.classList.add('row');
+//                     const headerRow = document.createElement('div');
+//                     headerRow.classList.add('sixteen');
+//                     headerRow.classList.add('wide');
+//                     headerRow.classList.add('column');
+//                     headerRow.classList.add('subject-name');
+//                     headerRow.setAttribute('id', subjectId);
+//                     const h2 = document.createElement('h2');
+//                     h2.classList.add('ui');
+//                     h2.classList.add('header');
+//                     h2.innerHTML = subjectName;
 
-    const playerRow = document.createElement("div");
-    playerRow.classList.add('row');
+//                     headerRow.appendChild(h2);
+//                     row.appendChild(headerRow);
 
-    // parse query strings
+//                     subcollections.map(subcollection => {
+//                         const { subcollectionName, title } = subcollection;
+//                         if(subcollectionName && title){
+//                             const listRow = document.createElement('div');
+//                             listRow.classList.add('sixteen');
+//                             listRow.classList.add('wide');
+//                             listRow.classList.add('column');    
+//                             const subHeaderRow = document.createElement('div');
+//                             subHeaderRow.classList.add('sixteen');
+//                             subHeaderRow.classList.add('wide');
+//                             subHeaderRow.classList.add('column');
+//                             subHeaderRow.classList.add('subheader-name');
+//                             const h3 = document.createElement('h3');
+//                             h3.classList.add('ui');
+//                             h3.classList.add('header');
+//                             h3.classList.add('curriculum-mbs');
+//                             h3.innerHTML = title;
+//                             const list = document.createElement('div');
+//                             list.classList.add('ui');
+//                             list.classList.add('relaxed');
+//                             list.classList.add('divided');
+//                             list.classList.add('list');
+
+//                             subHeaderRow.appendChild(h3);
+//                             listRow.appendChild(list);
+
+//                             row.appendChild(subHeaderRow);
+//                             row.appendChild(listRow);
+
+//                             circulumSubjects.appendChild(row);
+                            
+//                             const videosRef = db.collection('curriculum').doc(subjectName).collection(subcollectionName);
+//                             videosRef.get().then(videosSnapshot => {
+//                                 const videosDocs = videosSnapshot.docs;
+//                                 if(videosDocs){
+//                                     videosDocs.map(videoDoc => {
+//                                         const uid = videoDoc.id;
+//                                         const { title } = videoDoc.data();
+//                                         if(title){
+//                                             const item = document.createElement('div');
+//                                             item.classList.add('item');
+
+//                                             const content = document.createElement('div');
+//                                             content.classList.add('content');
+
+//                                             const header = document.createElement('a');
+//                                             header.innerHTML = title;
+//                                             header.classList.add('link');
+//                                             header.addEventListener('click', () => {
+//                                                 window.location = `curriculum-content.html?subject=${subjectName}&content=${subcollectionName}&uid=${uid}`;
+//                                             });
+
+//                                             content.appendChild(header);
+//                                             item.appendChild(content);
+//                                             list.appendChild(item);
+//                                         }
+//                                     })
+//                                 }
+                            
+//                             }).catch(err => {
+//                                 console.log(err);
+//                             })
+//                         }
+//                     });
+//                 }
+//             })
+//         }
+//     }).catch(err => {
+//         console.log(err);
+//     });
+// });
+$(document).ready(function(){
+
     const params = new URLSearchParams(window.location.search);
-    if(params.has('subject') && params.has('content') && params.has('uid')){
+
+    if(params.has('subject')){
         const subjectName = params.get('subject');
-        const content = params.get('content');
-        const uid = params.get('uid');
 
-        const contentRef = db.collection('curriculum').doc(subjectName).collection(content).doc(uid);
-        contentRef.get().then(contentSnapshot => {
-            if(contentSnapshot && contentSnapshot.exists){                
+        const db = firebase.firestore();
+        const curriculumRef = db.collection('curriculum').doc(subjectName);
+        curriculumRef.get().then(curriculumSnapshot => {
+            if(curriculumSnapshot && curriculumSnapshot.exists){
+                const circulumSubject = document.getElementById('circulum-subject');
+                const subjectName = curriculumSnapshot.id;
+                const subjectId = 'circulum-subject-title';
+                const { subcollections } = curriculumSnapshot.data();
+                if(subcollections){
+                    const row = document.createElement('div');
+                    row.classList.add('row');
 
-                const { title, embedID } = contentSnapshot.data();
+                    const h2 = document.createElement('h2');
+                    h2.classList.add('subject-name');
+                    h2.setAttribute('id', subjectId);
+                    h2.innerHTML = subjectName;
 
-                const p = document.createElement('p');
-                p.classList.add('center');
-                p.innerHTML = title;
+                    circulumSubject.appendChild(h2);
 
-                const playerDiv = document.createElement('div');
-                playerDiv.setAttribute("id", "player");
-                playerDiv.classList.add('plyr__video-embed');
+                    subcollections.map(subcollection => {
+                        const { subcollectionName, title } = subcollection;
+                        if(subcollectionName && title){
+                            const listRow = document.createElement('div');
+                            listRow.classList.add('sixteen');
+                            listRow.classList.add('wide');
+                            listRow.classList.add('column');    
+                            const subHeaderRow = document.createElement('div');
+                            subHeaderRow.classList.add('sixteen');
+                            subHeaderRow.classList.add('wide');
+                            subHeaderRow.classList.add('column');
+                            subHeaderRow.classList.add('subheader-name');
+                            const h3 = document.createElement('h3');
+                            h3.classList.add('curriculum-mbs');
+                            h3.innerHTML = title;
+                            const list = document.createElement('div');
+                            list.classList.add('ui');
+                            list.classList.add('relaxed');
+                            list.classList.add('divided');
+                            list.classList.add('list');
 
-                const iframe = document.createElement('iframe');
-                iframe.setAttribute("src", `https://www.youtube.com/embed/${embedID}?origin=https://plyr.io&amp;iv_load_policy=3&amp;modestbranding=1&amp;playsinline=1&amp;showinfo=0&amp;rel=0&amp;enablejsapi=1`);
-                iframe.setAttribute("allowfullscreen", "true");
-                iframe.setAttribute("allowtransparency", "true");
+                            subHeaderRow.appendChild(h3);
+                            listRow.appendChild(list);
 
-                playerDiv.appendChild(iframe);
+                            row.appendChild(subHeaderRow);
+                            row.appendChild(listRow);
 
-                headerRow.appendChild(p);
-                playerRow.appendChild(playerDiv);
+                            circulumSubject.appendChild(row);
+                            
+                            const videosRef = db.collection('curriculum').doc(subjectName).collection(subcollectionName);
+                            videosRef.get().then(videosSnapshot => {
+                                const videosDocs = videosSnapshot.docs;
+                                if(videosDocs){
+                                    videosDocs.map(videoDoc => {
+                                        const uid = videoDoc.id;
+                                        const { title } = videoDoc.data();
+                                        if(title){
+                                            const item = document.createElement('div');
+                                            item.classList.add('item');
 
-                contentDiv.appendChild(headerRow);
-                contentDiv.appendChild(playerRow);
-            } else {
-                window.location = 'curriculum.html';
+                                            const content = document.createElement('div');
+                                            content.classList.add('content');
+
+                                            const header = document.createElement('a');
+                                            header.innerHTML = title;
+                                            header.classList.add('link');
+                                            header.addEventListener('click', () => {
+                                                window.location = `curriculum-content.html?subject=${subjectName}&content=${subcollectionName}&uid=${uid}`;
+                                            });
+
+                                            content.appendChild(header);
+                                            item.appendChild(content);
+                                            list.appendChild(item);
+                                        }
+                                    })
+                                }
+                            }).catch(err => {
+                                console.log(err);
+                            })
+                        }
+                    });
+                }
             }
+        }).catch(err => {
+            console.log(err);
         });
     } else {
         window.location = 'curriculum.html';
