35a9074b5a0483ccb3dfef7f3cd2c894cf05a789
Gavin Hung <gavinkhung@gmail.com>
2021-05-21 22:28:48 -0700

    Started landing page

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,119 +3,76 @@ import { ReactNode, FC } from 'react';
 
 const IndexPage: FC = () => {
   return (
-    <>
-      <h1 className="text-4xl">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere quasi
-        aliquid quas, quod quae ducimus distinctio harum mollitia iusto possimus
-        laudantium corporis pariatur adipisci! Voluptas quod iusto ipsa sed,
-        voluptatem placeat ipsam quaerat dignissimos obcaecati amet fugit beatae
-        rem quasi possimus doloremque non in nisi consectetur architecto sit,
-        autem tempore ullam, similique quisquam! Incidunt, sapiente, tempora cum
-        sequi soluta illum magnam reprehenderit sit, assumenda ex nesciunt rerum
-        tenetur ad deleniti itaque ullam a natus asperiores sint quam odit
-        minima consectetur voluptatum recusandae? Saepe ipsa dolorum, tenetur
-        expedita unde qui dignissimos necessitatibus ut sint illo fugit quas
-        aspernatur nostrum! Laudantium iste vitae dolores distinctio adipisci
-        odit esse, magnam quibusdam tempora blanditiis explicabo nostrum ad quia
-        corporis architecto ullam magni non beatae eum numquam pariatur vero
-        aspernatur accusantium id. Porro, voluptates totam tempore non ratione
-        fugit repellat et autem repudiandae sapiente dolorum laudantium quasi
-        dolor quibusdam quam animi a fugiat. Architecto, et quis numquam error
-        voluptate neque libero adipisci officiis officia dolores animi
-        laudantium maxime voluptas eos. Distinctio sequi sunt corporis
-        accusantium, culpa consectetur accusamus magni tempora recusandae
-        architecto nemo vero delectus voluptatibus odit velit qui ea dolorum
-        possimus excepturi iste perspiciatis soluta eligendi non. Deserunt
-        corporis nam at doloribus. Est, iusto itaque minus dignissimos
-        laudantium accusamus doloremque totam numquam reiciendis quia aperiam
-        perspiciatis minima quod dolorum quis ducimus ex temporibus impedit
-        quibusdam repellendus odit. Iusto itaque facilis possimus cumque quam
-        hic quas eos delectus quae reiciendis? Voluptate, repellendus, enim
-        rerum consequatur, commodi voluptates repellat a dolore nesciunt
-        consectetur est nulla. Optio ut exercitationem minima consequuntur sed
-        maxime. Accusantium consequuntur laudantium aspernatur fuga suscipit
-        nobis quo impedit aliquam alias qui voluptatibus molestiae officia
-        praesentium quia explicabo iure maxime, ab ex maiores vel fugiat.
-        Voluptate placeat nostrum quis adipisci harum molestias quos,
-        accusantium incidunt dicta quisquam id. Provident, deleniti. Ab nulla
-        velit illo ea molestias alias similique architecto in eveniet non unde
-        cumque, nihil doloremque accusamus ex, nesciunt vero. Mollitia dolor
-        maxime sapiente, dolorem, dolores voluptatem at non officiis ullam
-        excepturi quidem quia cumque pariatur et autem provident soluta porro
-        dicta dolorum tempore doloribus quae ipsa eius! Quibusdam quisquam
-        necessitatibus qui adipisci quia harum, dolor at ab libero perspiciatis
-        dignissimos repellat dolorum dolores eum unde id suscipit, nihil enim
-        eveniet iusto consectetur illum distinctio. Officia iusto dolorem ut,
-        delectus dolores rerum nostrum. Optio officia aperiam eaque repellendus
-        perspiciatis nisi ipsum quam tempora rerum mollitia consequuntur facilis
-        quo nesciunt repudiandae, quos at magni explicabo blanditiis fugit ipsa
-        pariatur non quasi? Dolores unde sit earum voluptatibus suscipit ipsum
-        hic illum veniam accusamus repellendus excepturi, repellat, nemo ratione
-        nostrum laudantium. Minus vero sunt quae dolore. Nemo doloremque amet
-        sed dicta, obcaecati consequatur dolor blanditiis eos, eius consequuntur
-        perspiciatis laudantium officiis vero error nam nesciunt consectetur
-        mollitia. Fugit facere, atque neque tenetur animi aperiam consectetur
-        voluptatum ipsum nesciunt rerum minima. Quis quasi facere similique
-        itaque reiciendis magni iste numquam ratione quae, obcaecati dolorum
-        tempore suscipit velit nisi alias, maiores modi rem et accusantium! Quia
-        tempore aut distinctio, rerum maiores tempora corporis dignissimos
-        ipsam, atque est magnam amet accusamus veritatis. Delectus vero sint
-        alias a pariatur, quidem mollitia quos cum eaque voluptates quae,
-        excepturi quia numquam sequi dolorum nulla ex! Sed nihil nisi provident
-        iste blanditiis voluptatibus veniam ipsa? Accusamus natus magni
-        voluptatum enim, non atque quisquam reiciendis debitis. Mollitia in
-        cumque incidunt et aperiam quidem a doloremque reiciendis quasi. Hic,
-        iste nam quas, esse aut obcaecati voluptas saepe eaque commodi tempore
-        aliquam. Quae illum corrupti ipsa amet voluptates similique eaque omnis
-        vel quis tenetur dolor repellendus, dignissimos impedit numquam est
-        voluptatem a recusandae nisi sapiente cupiditate labore asperiores
-        molestiae dolores? Nobis distinctio consectetur quod pariatur? Veniam,
-        fuga! Aperiam saepe, dolorem illo in veritatis pariatur adipisci.
-        Voluptatibus rem optio esse ab iste commodi laboriosam atque fugit ipsam
-        vitae laborum consequuntur, ex beatae eius sequi nihil? Cupiditate
-        excepturi, repudiandae ipsam inventore molestiae similique explicabo
-        reprehenderit, blanditiis laborum, odio eum iusto tempora aspernatur
-        maxime corporis quaerat fugit eos voluptates nostrum id nesciunt eveniet
-        nemo deserunt ratione! Voluptatem omnis eum non est incidunt natus
-        similique ducimus iste labore, aut quae doloribus fugit rem architecto?
-        Quas autem quod reiciendis voluptatum, hic illo recusandae iste
-        voluptates beatae maiores. Sunt earum id quia. Aspernatur suscipit
-        asperiores soluta vel deleniti adipisci dolorum fugiat quidem
-        accusantium sed minus pariatur beatae rerum, nam consequuntur cupiditate
-        nesciunt itaque consequatur ipsam necessitatibus quos enim. At rem omnis
-        suscipit hic? Voluptates officiis, temporibus voluptatum aspernatur
-        iusto nesciunt qui? Corrupti ad obcaecati tempora necessitatibus
-        molestiae animi alias iure itaque fugiat non nemo fugit, saepe atque
-        deleniti voluptates placeat, ex quisquam doloremque corporis incidunt,
-        velit adipisci. Accusamus inventore facilis in, dignissimos pariatur
-        laborum omnis, quia sapiente deleniti dolores officiis culpa amet nobis
-        voluptates quos ducimus provident? Veniam maxime architecto eum
-        reiciendis cupiditate asperiores, perspiciatis repudiandae quidem illo
-        quaerat dolores sunt neque modi molestias culpa minus. Repudiandae,
-        perferendis voluptate fugiat commodi consequatur laborum adipisci quae
-        sint sit impedit animi, cumque laboriosam. Voluptas reiciendis porro
-        sapiente dolorem nihil quia non earum, ex explicabo nostrum tempora
-        voluptatum dolore aliquid, quaerat vel ab. Ipsa animi pariatur nulla
-        voluptate nihil at amet iste dolorem doloremque illo. Dicta ea est ad
-        eligendi sequi! Temporibus esse sapiente repellat, quas dolore totam
-        illo iusto dolor qui reprehenderit. Fugiat, sint illo! Perspiciatis
-        rerum harum earum nam error possimus in alias quam, magnam maiores
-        dolore? Officiis, quod distinctio! Reprehenderit deleniti tenetur sit
-        accusantium ratione, laudantium veritatis hic nesciunt quaerat
-        dignissimos sunt eius blanditiis. Libero non assumenda quam vel fugiat
-        saepe numquam eum reiciendis repellat commodi. Quibusdam ex iste nemo
-        molestias dicta placeat eveniet voluptate provident deleniti autem
-        doloribus fugit quia, quam culpa saepe libero rem ipsum facilis mollitia
-        excepturi voluptas? Laborum, incidunt quo et obcaecati sint provident
-        saepe, quaerat fugit corporis voluptas maiores deserunt voluptates earum
-        officiis? Earum rem itaque voluptate laboriosam autem omnis corporis
-        eius molestiae aperiam at tenetur vitae, officia, commodi suscipit, in
-        laborum ut praesentium. Officiis eveniet magni velit unde dolorum, dolor
-        quasi consequuntur qui adipisci officia voluptatum ut odit. Quas
-        aliquid, quos corporis, at a amet, illo voluptatem cupiditate officiis
-        deleniti alias obcaecati commodi distinctio id temporibus minus!
-      </h1>
-    </>
+    <div className="space-y-2">
+      <div className="flex flex-col sm:flex-row">
+        <div className="flex-1">
+          <h1 className="text-4xl">INNO</h1>
+          <h2>
+            A student run non profit organization that provides COMPETITIVE
+            OPPORTUNITIES to introduce students to biotech concepts and the
+            innovation behind them.
+          </h2>
+        </div>
+        <div className="flex-1">Home 1</div>
+      </div>
+      <div className="">
+        <p>
+          We are committed to providing our members with an ENRICHING, HOLISTIC,
+          HANDS-ON EXPERIENCE with biological and computational concepts,
+          fostering creativity in all our members and allowing them to use their
+          skills to make a real-world impact.
+        </p>
+      </div>
+      <div className="grid gap-4 grid-cols-1 sm:grid-cols-2 my-2 w-full">
+        <div className="flex flex-col justify-center bg-gray-400">
+          <h3>EXPERIENCE</h3>
+          <p>
+            Each member will be able to create a product directly applicable to
+            real world fields
+          </p>
+        </div>
+        <div className="flex flex-col justify-center bg-gray-400">
+          <h3>LEARNING</h3>
+          <p>
+            Robust curriculum and workshops will educate members about the
+            innovation behind biotech
+          </p>
+        </div>
+        <div className="flex flex-col justify-center bg-gray-400">
+          <h3>CONNECTIONS</h3>
+          <p>
+            We connect and allow members to foster relationships with industry
+            professionals
+          </p>
+        </div>
+        <div className="flex flex-col justify-center bg-gray-400">
+          <h3>RESOURCES</h3>
+          <p>
+            We provide members with the resources and tools necessary to create
+            impactful technology
+          </p>
+        </div>
+      </div>
+      <div className="">
+        <h2>MAKE AN IMAPCT</h2>
+        <div className="flex flex-row">
+          <div className="flex-1">
+            Create an Inno chapter at your school today to bring the innovation
+            that is paving the world of tomorrow to your school and community.{' '}
+          </div>
+          <div className="flex-1">img</div>
+        </div>
+      </div>
+      <div className="">
+        <h2>SPONSORS</h2>
+        <div className="flex flex-col sm:flex-row justify-center">
+          <div className="div">1</div>
+          <div className="div">2</div>
+          <div className="div">3</div>
+        </div>
+        <h3>WANT TO SUPPORT INNO?</h3>
+      </div>
+    </div>
   );
 };
 export default IndexPage;
